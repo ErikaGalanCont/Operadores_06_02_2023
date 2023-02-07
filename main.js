bb73ba6c33b45bf5f1347c93b88618b7a292db22
ErikaGalanCont <102776229+ErikaGalanCont@users.noreply.github.com>
2023-02-06 19:22:57 -0500

    Operadores Aritmeticos

---
+++ main.js
@@ -0,0 +1,47 @@
+let num1 = 50, num2 = 32;
+let resultado;
+let myStyle = "background: #0c690c;color: #fff;font-family: sans-serif;padding: 5px;border: 2px solid #0c6963;border-radius: 5px;";
+
+//Operador de adicion 'Suma'
+resultado = num1 + num2;
+console.log(`%c${num1} + ${num2} = ${resultado}`, myStyle);
+
+//Operador de subtracion 'Resta'
+resultado = num1 - num2;
+console.log(`%c${num1} - ${num2} = ${resultado}`, myStyle);
+
+//Operador de division
+resultado = num1 / num2;
+console.log(`%c${num1} - ${num2} = ${resultado}`, myStyle);
+
+//Operador de exponenciacion
+resultado = num1 ** num2;
+console.log(`%c${num1} - ${num2} = ${resultado}`, myStyle);
+
+//Operador de multiplicacion
+resultado = num1 + num2;
+console.log(`%c${num1} - ${num2} = ${resultado}`, myStyle);
+
+//Operador de residuo
+resultado = num1 % num2;
+console.log(`%c${num1} - ${num2} = ${resultado}`, myStyle);
+
+//Operador Unario Negativo
+num1 = -50;
+resultado = -num1;
+console.log(`%c${num1} - ${num2} = ${resultado}`, myStyle);
+
+//Operador Unirio Positivo
+num2 = 32;
+resultado = -num2;
+console.log(`%c${num1} - ${num2} = ${resultado}`, myStyle);
+
+
+
+
+
+
+
+
+
+
