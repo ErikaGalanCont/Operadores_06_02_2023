dd8d935c241e2cfb10c4c61ac176b6ead8fb9a9c
ErikaGalanCont <102776229+ErikaGalanCont@users.noreply.github.com>
2023-02-08 20:21:31 -0500

    operadores de comparacion

--- main.js
+++ main.js
@@ -2,58 +2,18 @@ let num1 = 50, num2 = 32;
 let resultado;
 let myStyle = "background: #0c690c;color: #fff;font-family: sans-serif;padding: 5px;border: 2px solid #0c6963;border-radius: 5px;";
 
-// Math.ceil(x)
-// Devuelve el entero más pequeño mayor o igual que un número.
-console.log(Math.ceil(3.01));
-//Redondea hacia arriba
+console.log(`%c${num1} == ${num2} ${num1==num2}`, myStyle);
 
-// Math.floor(x)
-// Devuelve el mayor entero menor que o igual a un número.
-console.log(Math.floor(2.9));
-//Redondea el numero hacia abajo
+console.log(`%c${num1} != ${num2} ${num1!=num2}`, myStyle);
 
-// Math.max()
-// Devuelve el mayor de cero o más números.
-console.log(Math.max(-1, -4, -45));
-//devuelve el numero mas alto de numeros dados
+console.log(`%c${num1} === ${num2} ${num1===num2}`, myStyle);
 
+console.log(`%c${num1} !== ${num2} ${num1!==num2}`, myStyle);
 
-// Math.min()
-// Devuelve el más pequeño de cero o más números.
-console.log(Math.min(-1, -4, -45));
-//devuelve el menor de los numeros dados
+console.log(`%c${num1} > ${num2} ${num1>num2}`, myStyle);
 
+console.log(`%c${num1} >= ${num2} ${num1>=num2}`, myStyle);
 
-// Math.pow(x, y)
-// Las devoluciones de base a la potencia de exponente, que es, baseexponent.
-console.log(Math.pow(3, 3));
+console.log(`%c${num1} < ${num2} ${num1<num2}`, myStyle);
 
-
-// Math.random()
-// Devuelve un número pseudo-aleatorio entre 0 y 1.
-console.log(Math.trunc()(Math.random()*3));
-//devuelve un numero aleatorio entre 0 y 1
-
-
-// Math.round(x)
-// Devuelve el valor de un número redondeado al número entero más cercano.
-console.log(Math.round(6.5));
-//Redondea hacia el entero que tiene mas cerca
-
-// Math.sqrt(x)
-// Devuelve la raíz cuadrada positiva de un número.
-console.log(Math.sqrt(16));
-//devuelve la raiz cuadrada
-
-
-// Math.trunc(x)
-// Devuelve la parte entera del número x, la eliminación de los dígitos fraccionarios.
-console.log(Math.trunc(42.84));
-//Quita el decimal
-
-// 5.68545.toFixed(x)
-// Devuelve la cantidad de numeros decimal segun x
-
-let num3 = 345.123456
-console.log(num3.toFixed(2));
-
+console.log(`%c${num1} <= ${num2} ${num1<=num2}`, myStyle);
