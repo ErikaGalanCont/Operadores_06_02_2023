1bb5fce55bc26ab81873d804c20cf03e44298478
ErikaGalanCont <102776229+ErikaGalanCont@users.noreply.github.com>
2023-02-13 22:02:34 -0500

    operador logico !

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 let a = confirm("Variable A\n\t\tSelecciona Aceptar si deseas el boleano True\n\t\tSelecciona Cancelar si deseas el booleano False")
-let b = confirm("Variable B\n\t\tSelecciona Aceptar si deseas el boleano True\n\t\tSelecciona Cancelar si deseas el booleano False")
+
 
 let cuerpo = document.querySelector("#myAND");
 cuerpo.insertAdjacentHTML("beforeend", `<tr>
                                             <td>${a}</td>
-                                            <td>${b}</td>
-                                            <td>${a || b}</td>
+                                            <td>${!a}</td>
                                         </tr>`);
